6ad5cfe252f6ee7b1620a65e71f8ed858ec4803a
Hootan Moradi <hoootaan@gmail.com>
2025-01-26 02:43:24 +0100

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,29 +1,44 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "CrudRouter",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v13)
     ],
     products: [
         .library(name: "CrudRouter", targets: ["CrudRouter"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.53.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.4.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.1.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.5.2"),
     ],
     targets: [
-      .target(name: "CrudRouter", dependencies: [
-        .product(name: "Vapor", package: "vapor"),
-        .product(name: "Fluent", package: "fluent"),
-        .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver")
-      ]),
-        .testTarget(name: "CrudRouterIntegrationTests", dependencies: [
-          .target(name: "CrudRouter"),
-          .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-          .product(name: "XCTVapor", package: "vapor")
-        ]),
-    ]
+        .target(
+            name: "CrudRouter",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Vapor", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "CrudRouterIntegrationTests",
+            dependencies: [
+                .target(name: "CrudRouter"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "XCTVapor", package: "vapor"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ],
+    swiftLanguageModes: [.v5]
 )
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
